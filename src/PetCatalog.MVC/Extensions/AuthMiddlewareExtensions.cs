5fd2b2b355079e9b455107a2762db551254b75a9
Ethan Shoham <ethan380s@gmail.com>
2021-07-12 16:52:21 +0300

    Works Better!

---
+++ src/PetCatalog.MVC/Extensions/AuthMiddlewareExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Builder;
+using PetCatalog.MVC.Middlewares;
+
+namespace PetCatalog.MVC.Extensions
+{
+    public static class AuthMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseAddAuthorization(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<AddAuthorizationMiddleware>();
+        }
+    }
+}
--- src/PetCatalog.MVC/Startup.cs
+++ src/PetCatalog.MVC/Startup.cs
@@ -6,14 +6,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using PetCatalog.Infra.Data.Contexts;
 using PetCatalog.MVC.Extensions;
-using System.Text.Json;
-using System;
-using System.Net;
-using PetCatalog.Application.Auth;
 using System.IO;
-using System.Diagnostics;
-using System.Text;
-using PetCatalog.Application.Interfaces;
 
 namespace PetCatalog.MVC
 {
@@ -43,68 +36,21 @@ namespace PetCatalog.MVC
             services.RegisterAutoMapper();
         }
 
-        public void Configure(IApplicationBuilder app, IAuthService authService, PetCatalogDbContext ctx, ImageFileContext fs)
+        public void Configure(IApplicationBuilder app, PetCatalogDbContext ctx, ImageFileContext fs)
         {
             //ctx.Database.EnsureDeleted();
             ctx.Database.EnsureCreated();
             //fs.Diractory.EnsureDeleted();
             fs.Diractory.EnsureCreated();
 
-            app.Use(async (context, next) =>
-            {
-                string token;
-                if (context.Request.Cookies.TryGetValue("accessToken", out token))
-                {
-                    if (!string.IsNullOrEmpty(token))
-                    {
-                        context.Request.Headers.Add("Authorization", "Bearer " + token);
-                    }
-                }
-                await next();
-            });
+
+            app.UseAddAuthorization();
 
             app.UseStaticFiles();
 
             app.UseRouting();
 
             app.UseAuthentication();
-
-            app.UseStatusCodePages(async context =>
-            {
-                var response = context.HttpContext.Response;
-                var httpContext = context.HttpContext;
-                if (response.StatusCode == (int)HttpStatusCode.Unauthorized || response.StatusCode == (int)HttpStatusCode.Forbidden)
-                {
-                    string refreshToken;
-
-                    if (!httpContext.Request.Cookies.TryGetValue("refreshToken", out refreshToken))
-                        response.Redirect($"/login?path={httpContext.Request.Path}");
-                    else
-                    {
-                        string accessToken;
-                        if(!httpContext.Request.Cookies.TryGetValue("accessToken", out accessToken))
-                            response.Redirect($"/login?path={httpContext.Request.Path}");
-                        else
-                        {
-                            if (refreshToken is null || accessToken is null)                           
-                                response.Redirect($"/login?path={httpContext.Request.Path}");                            
-                            else
-                            {
-                                var refreshRequest = new RefreshRequest() { RefreshToken = refreshToken, AccessToken = accessToken };
-                                var userWithTokens = authService.RefreshToken(refreshRequest);
-
-                                var options = new CookieOptions();
-                                options.Expires = userWithTokens.RefreshToken.ExpiryDate;
-
-                                httpContext.Response.Cookies.Delete("accessToken");                                
-                                httpContext.Response.Cookies.Append("accessToken", userWithTokens.AccessToken, options);
-
-                                response.Redirect(httpContext.Request.Path);
-                            }
-                        }                   
-                    }
-                }
-            });
 
             app.UseAuthorization();
 
